4810642d6258e459b6a1bb3940aa9a3d7c7bc121
cieniavska <katarzyna.cieniawska@gmail.com>
2019-11-28 11:59:48 +0100

    motivation range

--- src/js/components/Motivation.js
+++ src/js/components/Motivation.js
@@ -3,18 +3,31 @@ import React, {Component} from "react";
 class Motivation extends Component {
 
     state = {
-        msg: ''
-    }    
+        range: 50,
+    } 
+    
+    handleRange = (e) => {
+        this.setState({
+            range: e.target.value
+        })
+    }
 
     render() {
+        let message;
+        if (parseFloat(this.state.range) === 100) {
+            message = (<div className="motivation__container__msg">Wow! that is a lot!</div>)
+        }
+        else if (parseFloat(this.state.range) === 1) {
+            message = (<div className="motivation__container__msg">We need a little bit more from you!</div>)
+        }
         return (
         <section className="motivation">
             <div className="motivation__container flex">
                 <h2>Let's check out your motivation</h2>
                 <div className="rangeContainer">
-                    <input className="slider" type="range" min="1" max="100"></input>
+                    <input onChange={this.handleRange} name="range" value={this.state.range} className="slider" type="range" min="1" max="100"></input>
                 </div>
-                <div>{this.state.msg}</div>
+                {message}
             </div>
         </section>
         )
